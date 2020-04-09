b43207bddd7942d1a26133a16beb5b12449116f8
Mark Visschers <visschers.m@gmail.com>
2020-04-09 08:44:11 +0200

    Adding Select and projectto references

--- Services/BlogService.cs
+++ Services/BlogService.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System;
@@ -6,6 +7,7 @@ using MTech.DefaultMapping.ViewModel;
 using MTech.DefaultMapping.DataModel;
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
+using AutoMapper.Extensions;
 
 namespace MTech.DefaultMapping.Services
 {
@@ -25,32 +27,38 @@ namespace MTech.DefaultMapping.Services
         // For this we have a dependency on Entities
         public IEnumerable<Blog> GetEntities()
         {
-            return _repository.ToArray();
+            return _repository.AsNoTracking()
+                .ToArray();
         }
 
         public IEnumerable<BlogTitleView> GetViews()
         {
-            return _repository.Select(x => new BlogTitleView
-            {
-                
-            }).ToArray();
+            return _repository.AsNoTracking()
+                .Select(x => new BlogTitleView
+                {
+                    
+                }).ToArray();
         }
 
         // With AutoMapper and ProjectTo
         public IEnumerable<BlogTitleView> Get()
         {
-            return _repository.ProjectTo<BlogTitleView>(_mapper.ConfigurationProvider)
+            return _repository.AsNoTracking()
+                .ProjectTo<BlogTitleView>(_mapper.ConfigurationProvider)
                 .ToArray();
         }
+
         public IEnumerable<TView> Get<TView>()
         {
-            return _repository.ProjectTo<TView>(_mapper.ConfigurationProvider);
+            return _repository.AsNoTracking()
+                .ProjectTo<TView>(_mapper.ConfigurationProvider);
         }
 
         // Dependency on Entities
         public IEnumerable<TView> Get<TView>(Expression<Func<Blog, TView>> selectExpression)
         {
-            return _repository.Select(selectExpression).ToArray();
+            return _repository.AsNoTracking()
+                .Select(selectExpression).ToArray();
         }
     }
 }
